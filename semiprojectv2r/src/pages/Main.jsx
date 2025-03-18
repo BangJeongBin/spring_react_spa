ab4a665aeb3a070ea0e45d49f4c9a17dd9c0b85d
41817 <bangjungbin@gmail.com>
2025-03-18 11:29:46 +0900

    Main 출력

---
+++ semiprojectv2r/src/pages/Main.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+// Main 함수 컴포넌트 정의
+const Main = () => {
+    return (
+        <main id="content">
+            <img src="image/WALLPAPER.jpg" className="img-fluid" />
+        </main>
+    )
+}
+
+export default Main;
