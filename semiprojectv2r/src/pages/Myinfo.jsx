5ad420c42a5dbfef0e3707eb4d770e6fbde898de
41817 <bangjungbin@gmail.com>
2025-03-18 12:39:47 +0900

    Login 출력

---
+++ semiprojectv2r/src/pages/Myinfo.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import "../styles/member.css";
+
+// Myinfo 함수 컴포넌트 정의
+const Myinfo = () => {
+    return (
+        <main id="content">
+            <h2>회원정보</h2>
+            <div id="myinfo">
+
+                <table className="table table-bordered mb-0">
+                    <colgroup>
+                        <col style="width: 30%"/>
+                        <col/>
+                    </colgroup>
+                    <tbody>
+                    <tr>
+                        <td>아이디</td>
+                        <td><span></span></td>
+                    </tr>
+
+                    <tr>
+                        <td>이름</td>
+                        <td><span></span></td>
+                    </tr>
+
+                    <tr>
+                        <td>이메일</td>
+                        <td><span></span></td>
+                    </tr>
+
+                    <tr>
+                        <td>가입일</td>
+                        <td><span></span>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+
+            </div>
+        </main>
+    )
+}
+
+export default Myinfo;
