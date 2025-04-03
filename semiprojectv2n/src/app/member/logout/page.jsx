8ca2866d94f2d5031613c687ef6ca511669156fe
41817 <bangjungbin@gmail.com>
2025-04-03 10:39:32 +0900

    next.js에 AuthContext 로 인증정보 저장 구현 인증여부에 따라 로그인/로그아웃 표시 로그아웃 구현

---
+++ semiprojectv2n/src/app/member/logout/page.jsx
@@ -0,0 +1,14 @@
+'use client'
+
+import React from "react";
+
+const Login = () => {
+    // localStorage.clear();
+    localStorage.removeItem('accessToken');
+
+    return (
+        location.href = "/"
+    );
+}
+
+export default Login;
