8c5ac4a3cb6fe2df98c7570ca01b6e7d409d559a
41817 <bangjungbin@gmail.com>
2025-04-04 10:26:09 +0900

    갤러리 구현을 위한 JPA 관련 객체 생성

---
+++ SemiProjectV2/src/main/java/com/example/semiprojectv2/repository/GalleryRepository.java
@@ -0,0 +1,8 @@
+package com.example.semiprojectv2.repository;
+
+import com.example.semiprojectv2.domain.Gallery;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GalleryRepository extends JpaRepository<Gallery, Long> {
+
+}
