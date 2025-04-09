4b24dc19522ca3be649dc128da00f6afdfdc26a9
41817 <bangjungbin@gmail.com>
2025-04-09 10:31:21 +0900

    카카오 로그인에서 인가코드로 엑세스 토큰 발급 받기

--- SemiProjectV2/src/main/java/com/example/semiprojectv2/controller/KakaoController.java
+++ SemiProjectV2/src/main/java/com/example/semiprojectv2/controller/KakaoController.java
@@ -1,9 +1,10 @@
 package com.example.semiprojectv2.controller;
 
+import com.example.semiprojectv2.domain.KakaoTokenResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -26,7 +27,7 @@ public class KakaoController {
     @Value("${kakao.redirect.uri}")
     private String redirectUri;
 
-    private RestTemplate restTemplate;
+    private final RestTemplate restTemplate;
     private static String AccessToken = null;
 
 
@@ -48,8 +49,40 @@ public class KakaoController {
         log.info("인가 코드: " + code);
         
         // 2단계 : 엑세스 토큰 요청
+        String authorizeUrl = "https://kauth.kakao.com/oauth/token";
+        String params = "?client_id=%s&redirect_uri=%s&code=%s&grant_type=authorization_code";
+        String kakaoUrl = String.format(authorizeUrl + params, clientId, redirectUri, code);
 
-        return null;
+        // HTTP 헤더 설정
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+
+        // HTTP 요청 엔티티 생성
+        HttpEntity<String> request = new HttpEntity<>(headers);
+
+        try {
+            // POST 요청으로 토큰 받기
+            ResponseEntity<KakaoTokenResponse> response = restTemplate.postForEntity(
+                    kakaoUrl,
+                    request,
+                    KakaoTokenResponse.class // rest로 넘어온 데이터들을 클래스의 형태로 받아옴
+            );
+
+            // 응답에서 엑세스토큰 추출
+            KakaoTokenResponse tokenResponse = response.getBody();
+            if (tokenResponse != null) {
+                String accessToken = tokenResponse.getAccess_token();
+                log.info("Access Token : {}", accessToken);
+            }
+
+            return ResponseEntity.ok().body(tokenResponse);
+        } catch (Exception e) {
+            log.error("Error getting token: {}", e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Error getting token: " + e.getMessage());
+        }
+//        return null;
     }
 
     // 카카오 로그아웃
